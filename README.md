370de88a9662583698554bc017c5a644e5191aa9
Ujjaval Parmar <ujjavalparmar2002@gmail.com>
2024-03-26 23:52:54 +0530

    cleaning

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 **2023 nvim Config**
+
+###Packer link
+git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,7 +4,7 @@
 --})
 
 function ColorMyPencils(color)
-	color = color or "retrobox"
+	color = color or "desert"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
